76b5589603635e428db75065fcb1f96476da72a2
Harmen Mojet <h.mojet@scienta.nl>
2020-11-30 11:58:58 +0100

    First workable release

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# github-actions-env-toolkit
-Scienta's Github Actions env variables toolkit
+# Github Actions Environment variables Toolkit
+Scienta's Github Actions `env variables` toolkit
+
+## Exposed environment variables
+| Environment Variable   | Description                                                     | Example                   |
+|------------------------|-----------------------------------------------------------------|---------------------------|
+| `GITHUB_REF_NAME`      | Name of the branch or tag.                                      | `feature-develop/bug-fix` |
+| `GITHUB_REF_NAME_SLUG` | Sluggified version of the branch or tag names. Dot is allowed.  | `feature-develop-bug-fix` |
+| `GITHUB_SHA_SHORT`     | First eight characters of the full commit sha.                  | `f0425b57`                |
 
 # Build
 Start the docker container and login:
@@ -12,3 +19,6 @@ Install dependencies
 ```
 npm install --dev
 ```
+
+# Credits
+Based on [FranzDiebold/github-env-vars-action](https://github.com/FranzDiebold/github-env-vars-action).
--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,47 @@ module.exports =
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 const core = __webpack_require__(186);
-core.exportVariable('SCIENTA_TEST', "HALLOE");
+
+function getRefName(ref) {
+    return ref ? ref.split('/').slice(2).join('/') : null;
+}
+
+try {
+    const variables = {
+        GITHUB_REF_NAME: {
+            input: (env) => env.GITHUB_REF,
+            transform: (ref) => getRefName(ref)
+        },
+        GITHUB_REF_NAME_SLUG: {
+            input: (env) => env.GITHUB_REF,
+            transform: (ref) => getRefName(ref)
+                .toLowerCase()
+                .replace(/[^a-z0-9 -.]/g, ' ') // remove invalid chars. Allow a dot
+                .replace(/^\s+|\s+$/g, '') // trim
+                .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                .replace(/-+/g, '-') // collapse dashes
+        },
+        GITHUB_SHA_SHORT: {
+            input: (env) => env.GITHUB_SHA,
+            transform: (sha) => sha ? sha.substring(0, 8) : null
+        }
+    };
+
+    for (const [variableName, item] of Object.entries(variables)) {
+        const value = item.transform(
+            item.input(process.env)
+        );
+
+        if (value) {
+            core.exportVariable(variableName, value);
+            core.info(`Set ${variableName}=${value}`);
+        } else {
+            core.warning(`Could not set environment variable ${variableName}.`);
+        }
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
 
 
 /***/ }),
